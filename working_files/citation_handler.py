178897053f54637123ce5b7e37f0994dd745732f
Alessandro Rocchi <alessandro.rocchi13@studio.unibo.it>
2026-04-21 15:05:41 +0200

    Added OCI as ID

--- working_files/citation_handler.py
+++ working_files/citation_handler.py
@@ -8,7 +8,7 @@ my_graph = Graph()
 base_url = "https://schema.org/"
 
 #Classes Resources
-
+Identifier = URIRef(base_url + "identifier")
 Citation = URIRef(base_url + "citation")
 Author_SC = URIRef(base_url + "author_sc")
 Journal_SC = URIRef(base_url + "journal_sc")
@@ -37,20 +37,18 @@ citations = read_csv("data/dh_citations.csv",
                   })
 
 citations.columns = citations.columns.str.strip()
+citations["oci"] = citations["oci"].str.strip()
 citations["citing"] = citations["citing"].str.strip()
 citations["cited"] = citations["cited"].str.strip()
 
-citation_internal_oci = {}
-
 for idx, rows in citations.iterrows():
-    citation_internal_id = "citation_" + str(idx)
-    subj = URIRef(base_url + citation_internal_id)
+    
+    subj = URIRef(base_url + rows["oci"])    
     my_graph.add((subj, RDF.type, Citation))
     my_graph.add((subj, citing, URIRef(rows["citing"])))
     my_graph.add((subj, cited, URIRef(rows["cited"])))
     my_graph.add((subj, creation, Literal(rows["creation"])))
     my_graph.add((subj, timespan, Literal(rows["timespan"])))
-
     if rows["journal_sc"]:
         my_graph.add((subj, Journal_SC, Literal(rows["journal_sc"])))
     elif rows["author_sc"]:
